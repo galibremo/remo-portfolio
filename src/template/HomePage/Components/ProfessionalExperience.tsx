c0da40d6353bcabfb366eef890999f6e33bb031b
galibremo <galibremo@gmail.com>
2025-08-23 12:06:12 +0600

    feat: add Nextjs image for enhanced visual content

---
+++ src/template/HomePage/Components/ProfessionalExperience.tsx
@@ -0,0 +1,69 @@
+import { MapPin } from "lucide-react";
+import Image from "next/image";
+
+import { Card, CardContent } from "@/components/ui/card";
+
+export default function ProfessionalExperience() {
+	const experiences = [
+		{
+			image: "/uiux.jpg",
+			title: "UI/UX Designer (Trainee)",
+			company: "AKIJ iBOS Limited",
+			duration: "1 month",
+			description:
+				"Built a strong design sense and deeper understanding of user experience, forming the foundation for creating intuitive, user-friendly interfaces"
+		},
+		{
+			image: "/React.jpg",
+			title: "Software Engineer (React)",
+			company: "mPower Social",
+			duration: "Aug 2024 - Feb 2025",
+			description:
+				"Gaining real-world project experience, collaborate with talented colleagues,  understand workplace culture, strengthen my technical and teamwork skills"
+		},
+		{
+			image: "/Nextjs.jpg",
+			title: "Full-Stack Developer (Next js)",
+			company: "Typetech It",
+			duration: "Mar 2025 - Present",
+			description:
+				"Design and develop web applications using modern technologies.  Handle both front-end and back-end tasks while continuously learning new techonologies"
+		}
+	];
+
+	return (
+		<section>
+			<div className="mx-auto max-w-6xl px-4 py-10">
+				<div>
+					<h1 className="text-center text-4xl font-semibold">Professional Experience</h1>
+					<span className="block text-center text-lg">Internship and Jobs</span>
+				</div>
+				<div className="mt-12 flex flex-col items-center gap-6 md:flex-row">
+					{experiences.map((item, idx) => (
+						<Card key={idx} className="gap-2 rounded-md pt-0">
+							<Image
+								src={item.image}
+								alt={item.title}
+								height={500}
+								width={500}
+								className="h-50 w-full rounded-t-md p-0.5 object-cover"
+							/>
+							<CardContent>
+								<div className="mb-2">
+									<p className="mt-2 text-base font-medium sm:text-xl">{item.title}</p>
+									<div className="text-muted-foreground">
+										<span className="flex items-center gap-1">
+											<MapPin size={16} className="mb-0.5" />
+											{item.company} &nbsp;
+										</span>
+									</div>
+								</div>
+								<p className="text-sm">{item.description}</p>
+							</CardContent>
+						</Card>
+					))}
+				</div>
+			</div>
+		</section>
+	);
+}
--- src/template/HomePage/HomeTemplate.tsx
+++ src/template/HomePage/HomeTemplate.tsx
@@ -3,6 +3,7 @@ import ThemeToggle from "@/components/theme-toggle";
 import AboutMe from "@/template/HomePage/Components/AboutMe";
 import Education from "@/template/HomePage/Components/Education";
 import HeroSection from "@/template/HomePage/Components/HeroSection";
+import ProfessionalExperience from "@/template/HomePage/Components/ProfessionalExperience";
 import TopNavBar from "@/template/HomePage/Components/TopNavBar";
 
 export default function HomeTemplate() {
@@ -12,6 +13,7 @@ export default function HomeTemplate() {
 			<HeroSection />
 			<AboutMe />
 			<Education />
+			<ProfessionalExperience />
 			<div className="fixed right-4 bottom-4 z-50">
 				<ThemeToggle />
 			</div>
