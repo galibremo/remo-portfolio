27fd6b622a0258f406bdd0692f4a720a14fddab4
galibremo <galibremo@gmail.com>
2025-08-30 01:37:28 +0600

    feat: add new background image and enhance overlay opacity in HeroSection; introduce text gradient styling in TopNavBar

--- src/template/HomePage/Components/HeroSection.tsx
+++ src/template/HomePage/Components/HeroSection.tsx
@@ -15,27 +15,27 @@ export default function HeroSection() {
 			<div
 				className="absolute inset-0 z-0"
 				style={{
-					backgroundImage: "url('/hero-section-bg.jpg')",
+					backgroundImage: "url('/try2.jpg')",
 					backgroundSize: "cover",
 					backgroundRepeat: "no-repeat",
 					backgroundPosition: "center"
 				}}
 			>
 				{/* Dynamic overlay - black by default, white in dark mode */}
-				<div className="absolute inset-0 bg-black/15"></div>
+				<div className="absolute inset-0 bg-black/20"></div>
 			</div>
 
 			{/* Content */}
-			<div className="z-10 flex flex-col items-center gap-2">
+			<div className="z-10 flex flex-col items-center gap-1.5">
 				<Image
 					src="/hero-section-me.jpg"
 					alt="remo image"
 					priority
 					width={100}
 					height={100}
-					className="h-36 w-36 rounded-full border-4 border-white object-cover sm:h-46 sm:w-46 md:h-60 md:w-60 md:border-6"
+					className="h-36 w-36 rounded-full border-4 border-white object-cover sm:h-46 sm:w-46 md:h-50 md:w-50 md:border-6"
 				/>
-				<span className="mt-4 text-2xl font-medium text-white sm:text-3xl md:text-5xl">
+				<span className="mt-4 text-2xl font-medium text-white sm:text-3xl md:text-[44px]">
 					Hi, I&apos;m Galib Remo
 				</span>
 				<Typewriter
