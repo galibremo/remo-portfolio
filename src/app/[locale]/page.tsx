9d27eb2f52850d3717bcccabbb156d4caa7485a6
galibremo <galibremo@gmail.com>
2026-09-05 09:12:34 +0600

    refactor: remove console log from HomePage component
    - Eliminated debugging console log from HomePage to clean up code and improve performance.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,7 +6,5 @@ export const revalidate = 60;
 
 export default async function HomePage() {
 	const content = await getPortfolioContent();
-	console.log("Remo log >>>>>>>>>>>>>>",content);
-	
 	return <HomeTemplate content={content} />;
 }
